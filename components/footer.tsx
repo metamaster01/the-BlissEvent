87318e2a39d7060041bfa9c49e3cca32107c7351
Sujal <palsujal707@gmail.com>
2026-01-22 14:22:24 +0530

    update footer with links and responsiveness

--- components/footer.tsx
+++ components/footer.tsx
@@ -415,7 +415,7 @@ const Footer = () => {
               </a>
 
               <a
-                href="https://instagram.com"
+                href="https://www.instagram.com/blisseventsofficial/"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="social-icon w-10 h-10 bg-white/10 hover:bg-white hover:text-[#8B2E2E] rounded-full flex items-center justify-center transition-all duration-300 hover:scale-110 hover:-translate-y-1"
@@ -430,18 +430,23 @@ const Footer = () => {
               </a>
 
               <a
-                href="https://pinterest.com"
+                href="https://www.youtube.com/@theblissevents7433"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="social-icon w-10 h-10 bg-white/10 hover:bg-white hover:text-[#8B2E2E] rounded-full flex items-center justify-center transition-all duration-300 hover:scale-110 hover:-translate-y-1"
               >
-                <svg
+                {/* <svg
                   className="w-5 h-5"
                   fill="currentColor"
                   viewBox="0 0 24 24"
                 >
                   <path d="M12 0c-6.627 0-12 5.372-12 12 0 5.084 3.163 9.426 7.627 11.174-.105-.949-.2-2.405.042-3.441.218-.937 1.407-5.965 1.407-5.965s-.359-.719-.359-1.782c0-1.668.967-2.914 2.171-2.914 1.023 0 1.518.769 1.518 1.69 0 1.029-.655 2.568-.994 3.995-.283 1.194.599 2.169 1.777 2.169 2.133 0 3.772-2.249 3.772-5.495 0-2.873-2.064-4.882-5.012-4.882-3.414 0-5.418 2.561-5.418 5.207 0 1.031.397 2.138.893 2.738.098.119.112.224.083.345l-.333 1.36c-.053.22-.174.267-.402.161-1.499-.698-2.436-2.889-2.436-4.649 0-3.785 2.75-7.262 7.929-7.262 4.163 0 7.398 2.967 7.398 6.931 0 4.136-2.607 7.464-6.227 7.464-1.216 0-2.359-.631-2.75-1.378l-.748 2.853c-.271 1.043-1.002 2.350-1.492 3.146 1.124.347 2.317.535 3.554.535 6.627 0 12-5.373 12-12 0-6.628-5.373-12-12-12z" />
-                </svg>
+                </svg> */}
+
+                
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5">
+                  <path d="M21.543 7.2c-.2-1.4-1.4-2.5-2.8-2.7C16.9 4 12 4 12 4s-4.9 0-6.7.5c-1.4.2-2.6 1.3-2.8 2.7C2.5 9 2.5 12 2.5 12s0 3 .5 4.8c.2 1.4 1.4 2.5 2.8 2.7 1.8.5 6.7.5 6.7.5s4.9 0 6.7-.5c1.4-.2 2.6-1.3 2.8-2.7.5-1.8.5-4.8.5-4.8s0-3-.5-4.8zM10 15.5v-7l6 3.5-6 3.5z" />
+                </svg>  
               </a>
             </div>
           </div>
@@ -499,34 +504,42 @@ const Footer = () => {
               <ul className="space-y-3">
                 <li>
                   <a
-                    href="/services"
+                    href="/services/destination-wedding"
                     className="text-white/80 hover:text-white hover:translate-x-1 inline-block transition-all duration-300 text-sm"
                   >
-                    Weedings Celeberation
+                    Destination Wedding
                   </a>
                 </li>
                 <li>
                   <a
-                    href="/services"
+                    href="/services/resorts-hotel-booking"
                     className="text-white/80 hover:text-white hover:translate-x-1 inline-block transition-all duration-300 text-sm"
                   >
-                    Corporate Events
+                    Resorts & Hotel Booking
                   </a>
                 </li>
                 <li>
                   <a
-                    href="/services"
+                    href="/services/event-decoration"
                     className="text-white/80 hover:text-white hover:translate-x-1 inline-block transition-all duration-300 text-sm"
                   >
-                    Social Events
+                    Event Decoration
                   </a>
                 </li>
                 <li>
                   <a
-                    href="/services"
+                    href="/services/photography"
                     className="text-white/80 hover:text-white hover:translate-x-1 inline-block transition-all duration-300 text-sm"
                   >
-                    Event Decor
+                    Photography
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="/services/sangeet-entertainment"
+                    className="text-white/80 hover:text-white hover:translate-x-1 inline-block transition-all duration-300 text-sm"
+                  >
+                    Sangeet & Entertainment
                   </a>
                 </li>
               </ul>
@@ -695,7 +708,7 @@ const Footer = () => {
             <ul className="space-y-3">
               <li>
                 <a
-                  href="/home"
+                  href="/"
                   className="text-white/80 hover:text-white hover:translate-x-1 inline-block transition-all duration-300"
                 >
                   Home
@@ -742,34 +755,42 @@ const Footer = () => {
             <ul className="space-y-3">
               <li>
                 <a
-                  href="/services"
+                  href="/services/destination-wedding"
                   className="text-white/80 hover:text-white hover:translate-x-1 inline-block transition-all duration-300"
                 >
-                  Wedding Celebration
+                  Destination Wedding
                 </a>
               </li>
               <li>
                 <a
-                  href="/services"
+                  href="/services/resorts-hotel-booking"
                   className="text-white/80 hover:text-white hover:translate-x-1 inline-block transition-all duration-300"
                 >
-                  Corporate Events
+                  Resorts & Hotel Booking
                 </a>
               </li>
               <li>
                 <a
-                  href="/services"
+                  href="/services/event-decoration"
                   className="text-white/80 hover:text-white hover:translate-x-1 inline-block transition-all duration-300"
                 >
-                  Social Events
+                  Event decoration
                 </a>
               </li>
               <li>
                 <a
-                  href="/services"
+                  href="/services/photography"
                   className="text-white/80 hover:text-white hover:translate-x-1 inline-block transition-all duration-300"
                 >
-                  Event Decor
+                  Photography
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/services/sangeet-entertainment"
+                  className="text-white/80 hover:text-white hover:translate-x-1 inline-block transition-all duration-300"
+                >
+                  Sangeet & Entertainment
                 </a>
               </li>
             </ul>
